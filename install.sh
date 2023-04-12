7f3b39b8d4f4b1c86e6351c86a5ea7e180d7615b
COMS <1824461877@qq.com>
2023-04-12 18:40:26 +0800

    update mode

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-yum install mysql-client
-yum install curl
-yum install nginx
-curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
+#yum install mysql-client
+#yum install curl
+#yum install nginx
+#curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
 
 mkdir project
+cd project
+git clone git@github.com:1824461877/china-carbon.git
+git clone git@github.com:1824461877/rwen-back.git
+
